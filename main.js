1cdf09576e71fc1103260821db3253df50139a25
Sylwester <sylwestronowak@gmail.com>
2019-02-18 09:27:33 +0100

    completed navbar

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $('.carousel').carousel({
   interval: 3000
 })
+
+//Responsywny navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
