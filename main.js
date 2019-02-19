b2efba033917a10638bf947d4f19329ce0ec771d
Sylwester <sylwestronowak@gmail.com>
2019-02-19 10:55:37 +0100

    modal windows'

--- main.js
+++ main.js
@@ -23,3 +23,62 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+//Więcej informacji w sekcji informacji nad partnerami
+//Wawer
+var modal = document.getElementById('modal_1');
+var btn = document.getElementById("more_info_1");
+var span = document.getElementsByClassName("close1")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Bemowo
+var modal = document.getElementById('modal_2');
+var btn = document.getElementById("more_info_2");
+var span = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Mokotów
+var modal = document.getElementById('modal_3');
+var btn = document.getElementById("more_info_3");
+var span = document.getElementsByClassName("close3")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
