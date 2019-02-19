538223db000aa74f7fab3f7a71c792403713af93
Sylwester <sylwestronowak@gmail.com>
2019-02-19 12:09:09 +0100

    more info modal box complete

--- main.js
+++ main.js
@@ -25,47 +25,41 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-//Więcej informacji w sekcji informacji nad partnerami
-//Wawer
-document.getElementById("more_info_1").onclick = function() {
-  document.getElementById('modal_1').style.display = "block";
-}
-
-span.onclick = function() {
-  document.getElementById('modal_1').style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == document.getElementById('modal_1')) {
-    document.getElementById('modal_1').style.display = "none";
+//Więcej informacji w sekcji informacji nad partnerami (przycisk)
+  var modal1 = document.getElementById('modal1');
+  var modal2 = document.getElementById('modal2');
+  var modal3 = document.getElementById('modal3');
+  var btn1 = document.getElementById("more_info1");
+  var btn2 = document.getElementById("more_info2");
+  var btn3 = document.getElementById("more_info3");
+  var span0 = document.getElementsByClassName("close")[0];
+  var span1 = document.getElementsByClassName("close")[1];
+  var span2 = document.getElementsByClassName("close")[2];
+  btn1.onclick = function() {
+    modal1.style.display = "block";
   }
-}
-
-//Bemowo
-document.getElementById("more_info_2").onclick = function() {
-  document.getElementById('modal_2').style.display = "block";
-}
-
-span.onclick = function() {
-  document.getElementById('modal_2').style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == document.getElementById('modal_2')) {
-    document.getElementById('modal_2').style.display = "none";
+  btn2.onclick = function() {
+    modal2.style.display = "block";
   }
-}
-//Mokotów
-document.getElementById("more_info_3").onclick = function() {
-  document.getElementById('modal_1').style.display = "block";
-}
-
-span.onclick = function() {
-  document.getElementById('modal_3').style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == document.getElementById('modal_3')) {
-    document.getElementById('modal_3').style.display = "none";
+  btn3.onclick = function() {
+    modal3.style.display = "block";
   }
-}
+
+  span0.onclick = function() {
+    modal1.style.display = "none";
+  }
+  span1.onclick = function() {
+    modal2.style.display = "none";
+  }
+  span2.onclick = function() {
+    modal3.style.display = "none";
+  }
+
+
+  window.onclick = function(event) {
+    if (event.target == modal1 || event.target == modal2 || event.target == modal3) {
+      modal1.style.display = "none";
+      modal2.style.display = "none";
+      modal3.style.display = "none";
+    }
+  }
